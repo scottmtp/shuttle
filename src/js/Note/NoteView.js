eb23498697ffede164f283f9e05bf9a770c670a5
Scott Dietrich <scdietrich@gmail.com>
2015-07-27 07:34:42 -0700

    Improve event handling of document saves and updates.

--- src/js/Note/NoteView.js
+++ src/js/Note/NoteView.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Styles, Tab, Tabs, TextField } from 'material-ui';
+import _ from 'lodash';
 
+import NavViewActions from '../NavViewActions';
 import NoteStore from './NoteStore';
 import NoteViewActions from './NoteViewActions';
 
@@ -9,10 +11,14 @@ export default class NoteView extends React.Component {
     super(props);
     this.state = { note: NoteStore.getCurrentNote() };
     
+    this._onSquireLoad = this._onSquireLoad.bind(this);
     this._onChange = this._onChange.bind(this);
     this.handleDocumentChange = this.handleDocumentChange.bind(this);
     this.handleTitleChange = this.handleTitleChange.bind(this);
-    this._onSquireLoad = this._onSquireLoad.bind(this);
+    this.handleNewState = this.handleNewState.bind(this);
+    
+    this.handleDocUpdate = _.throttle(this.handleDocUpdate.bind(this), 200);
+    this.handleNavUpdate = _.throttle(this.handleNavUpdate.bind(this), 200);
   }
 
   componentDidMount() {
@@ -21,6 +27,23 @@ export default class NoteView extends React.Component {
 
   componentWillUnmount() {
     NoteStore.removeChangeListener(this._onChange);
+  }
+  
+  handleNavUpdate() {
+    NavViewActions.update();
+  }
+  
+  handleDocUpdate(groupId, noteId, title, markup) {
+    NoteViewActions.updateNote(groupId, noteId, title, markup);
+  }
+  
+  handleNewState(title, markup) {
+    this.setState({
+      note: {
+        html: markup,
+        title: title
+      }
+    });
   }
   
   _onChange() {
@@ -41,27 +64,22 @@ export default class NoteView extends React.Component {
   }
   
   handleTitleChange(e) {
-    this.setState({
-      note: {
-        html: this.state.note.html,
-        title: e.target.value
-      }
-    });
+    var newTitle = e.target.value;
     
-    NoteViewActions.updateNote(this.props.params.groupid, this.props.params.noteid,
-      e.target.value, this.state.note.html);
+    this.handleNewState(newTitle, this.state.note.html);
+    this.handleDocUpdate(this.props.params.groupid, this.props.params.noteid,
+      newTitle, this.state.note.html);
+      
+    // delay update of left nav to ensure document was written
+    _.delay(this.handleNavUpdate, 200);
   }
   
   handleDocumentChange() {
-    this.setState({
-      note: {
-        html: this.editor.getHTML(),
-        title: this.state.note.title
-      }
-    });
+    var newHtml = this.editor.getHTML();
     
-    NoteViewActions.updateNote(this.props.params.groupid, this.props.params.noteid,
-      this.state.note.title, this.state.note.html);
+    this.handleNewState(this.state.note.title, newHtml);
+    this.handleDocUpdate(this.props.params.groupid, this.props.params.noteid,
+      this.state.note.title, newHtml);
   }
   
   getStyles() {
