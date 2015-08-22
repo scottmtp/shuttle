b8e25dd00558e6bb3fdbacceacc81438592b5aed
Scott Dietrich <scdietrich@gmail.com>
2015-08-22 15:13:18 -0700

    Reload project list when new accounts are initialized. Adjust new account documents.

--- src/js/NewAccountAPI.js
+++ src/js/NewAccountAPI.js
@@ -1,8 +1,10 @@
 import uuid from 'uuid';
 import dbApi from './DbAPI';
 
+import ProjectViewActions from './Project/ProjectViewActions';
+
 export default {
-  
+
   initializeNewAccount: function() {
     console.log('initializeNewAccount');
     let project = {
@@ -13,38 +15,33 @@ export default {
 
     let note1 = {
       _id: uuid.v4(),
-      title: 'Getting Started',
-      html: '<h1>Welcome to Getting Started!</h1>'
-    };
-  
-    let note2 = {
-      _id: uuid.v4(),
       title: 'Notes',
       html: '<h1>Welcome to Notes!</h1>'
     };
-    
+
     let list1 = {
       _id: uuid.v4(),
       title: 'Shopping'
     };
-  
+
     let list2 = {
       _id: uuid.v4(),
       title: 'Todo'
     };
-  
+
     return dbApi.updateGroup(project)
       .then(function() {
         return dbApi.updateNote(project, note1);
-      })
-      .then(function() {
-        return dbApi.updateNote(project, note2);
       })
       .then(function() {
         return dbApi.updateList(project, list1);
       })
       .then(function() {
         return dbApi.updateList(project, list2);
+      })
+      .then(function() {
+        // TODO: revisit how new accounts are initialized
+        ProjectViewActions.getProjects();
       });
   }
 };
