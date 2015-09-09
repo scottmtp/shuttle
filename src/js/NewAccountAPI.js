ece6955ba4faa150db8e5c4e1529a06462e973f0
Scott Dietrich <scdietrich@gmail.com>
2015-09-09 12:05:38 -0700

    Update new account note text.

--- src/js/NewAccountAPI.js
+++ src/js/NewAccountAPI.js
@@ -1,5 +1,6 @@
 import uuid from 'uuid';
 import dbApi from './DbAPI';
+import DbTypes from './DbTypes';
 
 import ProjectViewActions from './Project/ProjectViewActions';
 
@@ -15,8 +16,23 @@ export default {
 
     let note1 = {
       _id: uuid.v4(),
-      title: 'Notes',
-      html: '<div>Welcome to Notes! You can edit this text by clicking on the Edit tab.</div>'
+      title: 'Getting Started',
+      html: `<div>
+<div><span style="font-size: 18px;"><b>Welcome to Shuttle!</b></span></div>
+<div><br></div>
+<div>Shuttle is a Todo and Note taking app with a focus on privacy. Data
+is stored locally and is always in your control. We do not store your
+data on our servers, even when Sharing is enabled. Data will only sync
+when multiple devices with the same Sharing key are online.</div>
+<div><br></div>
+<div>Also, Shuttle has been optimized to work well offline and on devices of all sizes.</div>
+<div><br></div>
+<div>You can access Lists and Notes from the top-left menu, and you can
+customize Shuttle on the Projects page!</div>
+<div><br></div>
+<div>Also, if you have any feedback or comments about Shuttle, we would&nbsp;
+<a href="mailto:info@tryshuttle.com?subject=Shuttle feedback">love to hear from you!</a></div>
+</div>`
     };
 
     let list1 = {
@@ -40,8 +56,10 @@ export default {
         return dbApi.updateList(project, list2);
       })
       .then(function() {
-        // TODO: revisit how new accounts are initialized
         ProjectViewActions.getProjects();
+        // $localStorage.group = project._id;
+        // $localStorage.type = DbTypes.TYPE_NOTE;
+        // $localStorage.typeId = note1._id;
       });
   }
 };
